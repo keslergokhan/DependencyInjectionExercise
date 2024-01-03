52afc4bf3f0b40c8aef5d6e1837a1ce99f91fd1c
Gökhan KESLER <g.kesler@nexum.com.tr>
2024-01-04 00:48:27 +0300

    Fix

--- src/Core/Core.Application/Attributes/DependencyRegisterAttribute.cs
+++ src/Core/Core.Application/Attributes/DependencyRegisterAttribute.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace Core.Application.Attributes
 {
+    /// <summary>
+    /// Microsoft.Extensions.DependencyInjection yapısına bağımlılıklarımızı dinamik bir şeklide uygulayabilmemize olanak tanır,
+    /// öznitelik kayıt DI içerisine kayıt edilmesini istediğimiz sınıf için uygulanır.
+    /// </summary>
     public class DependencyRegisterAttribute : Attribute
     {
+        /// <summary>
+        /// Uygulanan arabirim tipi
+        /// </summary>
         public Type InterfaceType {  get; set; }
+        /// <summary>
+        /// DI kayıt tipi
+        /// </summary>
         public DependencyTypes DependencyTypes { get; set; }
 
         public DependencyRegisterAttribute(Type ınterfaceType, DependencyTypes dependencyTypes)
--- src/Core/Core.Application/ServiceRegistration.cs
+++ src/Core/Core.Application/ServiceRegistration.cs
@@ -15,14 +15,19 @@ namespace Core.Application
     {
         public static void ApplicationServiceInitialize(this IServiceCollection serviceDescriptors)
         {
+            //Mevcut assembly içerisine tüm type değerlerini kontrolet ve bana sadece 'DependencyRegisterAttribute' özniteliğini uygulayan sınıfları getir
             Assembly.GetExecutingAssembly().GetTypes()?.Where(x => x.GetCustomAttribute<DependencyRegisterAttribute>() != null).ToList()?.ForEach(classType =>
             {
+                //öznitelik içerisindeki property değerlerine erişmek için özniteliğe eriştik
                 DependencyRegisterAttribute dependencyRegisterAttribute = classType.GetCustomAttribute<DependencyRegisterAttribute>();
+                //öznitelik de tanımlanan interface tipi sayesinde mevcut class içerisinde uygulanan arabirimlerden ihtiyacımız olanın tipine eriştik
                 Type interfaceType = classType.GetInterface(dependencyRegisterAttribute.InterfaceType.Name);
 
+                //eğer arabirim bulunamadıysa anlaşılabilmesi için özel bir exception tanımladık.
                 if (interfaceType == null)
                     throw new InterfaceNotFoundException(classType.Name,dependencyRegisterAttribute.InterfaceType.Name);
 
+                //ihtiyacımıza göre kayıt işlemi yaptık.
                 if (dependencyRegisterAttribute.DependencyTypes == DependencyTypes.Scopet)
                 {
                     serviceDescriptors.AddScoped(interfaceType,classType);
